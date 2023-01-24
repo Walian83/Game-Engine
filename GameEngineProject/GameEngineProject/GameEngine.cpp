0c543b5f910fbd6a7e5e45f07be4011e9ffd64d3
Krystian Walus <Krystian.Walus@mail.bcu.ac.uk>
2023-01-24 14:57:05 +0000

    Defined the GameEngine::init() method
    - Added code to the method
    - Changed the SDL version to the correct one

--- GameEngineProject/GameEngineProject/GameEngine.cpp
+++ GameEngineProject/GameEngineProject/GameEngine.cpp
@@ -1,2 +1,72 @@
 #include "GameEngine.h"
 #include <iostream>
+
+namespace GE {
+	GameEngine::GameEngine() {
+
+	}
+
+	GameEngine::~GameEngine() {
+
+	}
+
+	bool GameEngine::init() {
+		// Initialise SDL video subsystem
+		if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+			std::cerr << "Unable to initialise SDL! SDL error: " << SDL_GetError() << std::endl;
+			
+			return false;
+		}
+		// Set the OpenGL version for the program
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+		// Set the type of profile which is core meaning modern OpenGL
+		// means no legacy features for backwards compatibility
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+		// Create the window and frame features
+		// In this case window has a fixed size and prepares window for OpenGL to render into
+		window = SDL_CreateWindow("SDL OpenGL", 50, 50, 640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+
+		// Check window was created
+		if (window == nullptr) {
+			// Display an error if window wasn't created
+			//
+			//
+			std::cerr << "Unable to create window! SDL error: " << SDL_GetError() << std::endl;
+
+			// Return failure so caller can work out what to do
+			return false;
+		}
+		//
+		//
+		glContext = SDL_GL_CreateContext(window);
+
+		//
+		if (glContext == nullptr) {
+			//
+			std::cerr << "SDL could not create GL context! SDL error: " << SDL_GetError() << std::endl;
+			return false;
+		}
+		//
+		//
+		//
+		GLenum status = glewInit();
+
+		//
+		if (status != GLEW_OK) {
+			std::cerr << "Error initialising GLEW! Error: " << glewGetErrorString(status) << std::endl;
+
+			return false;
+		}
+		//
+		//
+		if (SDL_GL_SetSwapInterval(1) != 0) {
+			std::cerr << "Warning: unable to set VSync! Error: " << SDL_GetError() << std::endl;
+
+			return false;
+		}
+		//
+		return true;
+	}
+}
