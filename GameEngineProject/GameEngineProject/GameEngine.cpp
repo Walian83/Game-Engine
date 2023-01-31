952aa8b2f7bb6ab33fbda77171a2a925486897f8
Krystian Walus <Krystian.Walus@mail.bcu.ac.uk>
2023-01-31 15:10:22 +0000

    Added shapes
    Added codes for more shapes

--- GameEngineProject/GameEngineProject/GameEngine.cpp
+++ GameEngineProject/GameEngineProject/GameEngine.cpp
@@ -101,20 +101,42 @@ namespace GE {
 		glClearColor(0.392f, 0.584f, 0.929f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glBegin(GL_TRIANGLES);
+		/*glBegin(GL_TRIANGLES);
 			glColor3f(1.0f, 0.0f, 0.0f);
 			glVertex2f(-1.0f, 0.0f);
-			glColor3f(1.0f, 0.0f, 0.0f);
+			glColor3f(0.0f, 1.0f, 0.0f);
 			glVertex2f(1.0f, 0.0f);
-			glColor3f(1.0f, 0.0f, 0.0f);
+			glColor3f(0.0f, 0.0f, 1.0f);
 			glVertex2f(0.0f, 1.0f);
 
-			glColor3f(0.0f, 1.0f, 0.0f);
+			glColor3f(1.0f, 0.0f, 0.0f);
 			glVertex2f(-1.0f, 0.0f);
-			glColor3f(0.0f, 1.0f, 0.0f);
+			glColor3f(0.0f, 0.0f, 1.0f);
 			glVertex2f(0.0f, -1.0f);
 			glColor3f(0.0f, 1.0f, 0.0f);
-			glVertex2f(1.0f, 0.0f);
+			glVertex2f(1.0f, 0.0f);*/
+
+		/*glBegin(GL_POLYGON);
+		glColor3f(0.0f, 0.0f, 1.0f);
+		glVertex2f(-0.75, -0.75);
+
+		glColor3f(0.0f, 0.0f, 1.0f);
+		glVertex2f(0.75, -0.75);
+
+		glColor3f(0.0f, 0.0f, 1.0f);
+		glVertex2f(0.75, 0.75);
+
+		glColor3f(0.0f, 0.0f, 1.0f);
+		glVertex2f(-0.75, 0.75);*/
+
+		// Vertex3f
+		glBegin(GL_TRIANGLES);
+			glColor3f(1.0f, 0.0f, 0.0f);
+			glVertex3f(-1.0f, 0.0f, 0.0f);
+			glColor3f(0.0f, 1.0f, 0.0f);
+			glVertex3f(1.0f, 0.0f, 0.0f);
+			glColor3f(0.0f, 0.0f, 1.0f);
+			glVertex3f(0.0f, 1.0f, 0.0f);
 
 		glEnd();
 
